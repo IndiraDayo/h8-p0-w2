99d82d53004e04ea0046df41373f4a4a0da5bd8f
IndiraDayo <indiradayo@live.com>
2018-04-17 17:04:42 +0700

    Revised

--- exercise-6.js
+++ exercise-6.js
@@ -1,5 +1,3 @@
-//Tugas 1
-
 console.log('LOOPING PERTAMA')
 
 var i = 0;
@@ -45,10 +43,35 @@ for (var i = 0; i < 10; i++) {
   }
 }
 
-for (var i = 1; i < 10; i += 2) {
+console.log(' ')
+
+
+for (var i = 1; i < 100; i += 2) {
   if (i % 3 === 0) {
-    console.log(i + ' kelipatan ' + i);
+    console.log(i + ' kelipatan ' + 3);
   } else {
-    console.log('" "');
+    console.log('""');
+  }
+}
+
+console.log(' ')
+
+
+for (var i = 1; i < 100; i += 5) {
+  if (i % 6 === 0) {
+    console.log(i + ' kelipatan ' + 6);
+  } else {
+    console.log('""');
+  }
+}
+
+console.log(' ')
+
+
+for (var i = 1; i < 100; i += 9) {
+  if (i % 10 === 0) {
+    console.log(i + ' kelipatan ' + 10);
+  } else {
+    console.log('""');
   }
 }
